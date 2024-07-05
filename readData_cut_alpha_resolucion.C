8678157987d70212a7aec43f45b8fba34dcb7dfc
Daniel Movilla Quintero <150165631+DanielMovQui@users.noreply.github.com>
2024-07-05 11:01:06 +0200

    Update readData_cut_alpha_resolucion.C

--- readData_cut_alpha_resolucion.C
+++ readData_cut_alpha_resolucion.C
@@ -1,1 +1,229 @@
+Float_t Ex = 0;
+Int_t detID = 0;
+Float_t coinTime = 0;
+Float_t e[24];
+Float_t rdt[8];
+Float_t x = 0;
+Float_t thetaCM = 0;
 
+void readData_cut_alpha()
+{
+TFile *f = new TFile("h082_10BDP_trace_run013_015-019_025-041.root");
+TTree *tree = (TTree*)f->Get("tree");
+
+tree->SetBranchAddress("Ex",&Ex);
+tree->SetBranchAddress("e", e);
+tree->SetBranchAddress("rdt", rdt);
+tree->SetBranchAddress("detID", &detID);
+tree->SetBranchAddress("coinTime", &coinTime);
+tree->SetBranchAddress("x", &x);
+tree->SetBranchAddress("thetaCM", &thetaCM);
+
+tree->SetBranchStatus("*", 0);
+tree->SetBranchStatus("Ex", 1);
+tree->SetBranchStatus("x", 1);
+tree->SetBranchStatus("rdt", 1);
+tree->SetBranchStatus("thetaCM", 1);
+tree->SetBranchStatus("coinTime", 1);
+tree->SetBranchStatus("detID", 1);
+tree->SetBranchStatus("e", 1);
+
+auto cutLitium1 = new TCutG("CUTLITIUM1",10);
+   cutLitium1->SetVarX("rdtH[0]");
+   cutLitium1->SetVarY("");
+   cutLitium1->SetTitle("Graph");
+   cutLitium1->SetFillStyle(1000);
+   cutLitium1->SetPoint(0,82.5365,3299.11);
+   cutLitium1->SetPoint(1,386.778,1441.96);
+   cutLitium1->SetPoint(2,1321.24,584.821);
+   cutLitium1->SetPoint(3,3461.8,263.393);
+   cutLitium1->SetPoint(4,4037.68,424.107);
+   cutLitium1->SetPoint(5,3374.87,888.393);
+   cutLitium1->SetPoint(6,1245.18,1316.96);
+   cutLitium1->SetPoint(7,93.4023,3299.11);
+   cutLitium1->SetPoint(8,82.5365,3227.68);
+   cutLitium1->SetPoint(9,82.5365,3299.11);
+   cutLitium1->Draw("");
+
+auto cutLitium3 = new TCutG("CUTLITIUM3",8);
+   cutLitium3->SetVarX("rdtH[2]");
+   cutLitium3->SetVarY("");
+   cutLitium3->SetTitle("Graph");
+   cutLitium3->SetFillStyle(1000);
+   cutLitium3->SetPoint(0,267.255,2129.46);
+   cutLitium3->SetPoint(1,3689.98,540.179);
+   cutLitium3->SetPoint(2,2798.98,236.607);
+   cutLitium3->SetPoint(3,1038.73,629.464);
+   cutLitium3->SetPoint(4,256.389,2165.18);
+   cutLitium3->SetPoint(5,441.107,2004.46);
+   cutLitium3->SetPoint(6,419.376,2040.18);
+   cutLitium3->SetPoint(7,267.255,2129.46);
+   cutLitium3->Draw("");
+
+auto cutLitium2 = new TCutG("CUTLITIUM2",7);
+   cutLitium2->SetVarX("rdtH[1]");
+   cutLitium2->SetVarY("");
+   cutLitium2->SetTitle("Graph");
+   cutLitium2->SetFillStyle(1000);
+   cutLitium2->SetPoint(0,202.06,2084.82);
+   cutLitium2->SetPoint(1,3461.8,531.25);
+   cutLitium2->SetPoint(2,2549.07,334.821);
+   cutLitium2->SetPoint(3,930.068,727.679);
+   cutLitium2->SetPoint(4,136.865,1977.68);
+   cutLitium2->SetPoint(5,245.523,2102.68);
+   cutLitium2->SetPoint(6,202.06,2084.82);
+   cutLitium2->Draw("");
+
+auto cutLitium4 = new TCutG("CUTLITIUM4",6);
+   cutLitium4->SetVarX("rdtH[3]");
+   cutLitium4->SetVarY("");
+   cutLitium4->SetTitle("Graph");
+   cutLitium4->SetFillStyle(1000);
+   cutLitium4->SetPoint(0,147.731,2040.18);
+   cutLitium4->SetPoint(1,3776.9,575.893);
+   cutLitium4->SetPoint(2,3092.36,272.321);
+   cutLitium4->SetPoint(3,886.605,736.607);
+   cutLitium4->SetPoint(4,158.597,2093.75);
+   cutLitium4->SetPoint(5,147.731,2040.18);
+   cutLitium4->Draw("");
+
+auto cutalpha1 = new TCutG("CUTALPHA1",7);
+   cutalpha1->SetVarX("rdtH[0]");
+   cutalpha1->SetVarY("");
+   cutalpha1->SetTitle("Graph");
+   cutalpha1->SetFillStyle(1000);
+   cutalpha1->SetPoint(0,39.0733,1120.54);
+   cutalpha1->SetPoint(1,39.0733,102.679);
+   cutalpha1->SetPoint(2,2896.78,138.393);
+   cutalpha1->SetPoint(3,1527.69,513.393);
+   cutalpha1->SetPoint(4,28.2075,1120.54);
+   cutalpha1->SetPoint(5,39.0733,1120.54);
+   cutalpha1->SetPoint(6,39.0733,1120.54);
+   cutalpha1->Draw("");
+
+auto cutalpha3 = new TCutG("CUTALPHA3",8);
+   cutalpha3->SetVarX("rdtH[2]");
+   cutalpha3->SetVarY("");
+   cutalpha3->SetTitle("Graph");
+   cutalpha3->SetFillStyle(1000);
+   cutalpha3->SetPoint(0,2766.39,254.464);
+   cutalpha3->SetPoint(1,701.886,736.607);
+   cutalpha3->SetPoint(2,104.268,1522.32);
+   cutalpha3->SetPoint(3,17.3418,1058.04);
+   cutalpha3->SetPoint(4,202.06,165.179);
+   cutalpha3->SetPoint(5,2733.79,129.464);
+   cutalpha3->SetPoint(6,2733.79,290.179);
+   cutalpha3->SetPoint(7,2766.39,254.464);
+   cutalpha3->Draw("");
+
+auto cutalpha2 = new TCutG("CUTALPHA2",8);
+   cutalpha2->SetVarX("rdtH[1]");
+   cutalpha2->SetVarY("");
+   cutalpha2->SetTitle("Graph");
+   cutalpha2->SetFillStyle(1000);
+   cutalpha2->SetPoint(0,115.134,1227.68);
+   cutalpha2->SetPoint(1,1799.33,424.107);
+   cutalpha2->SetPoint(2,2657.73,245.536);
+   cutalpha2->SetPoint(3,2049.24,120.536);
+   cutalpha2->SetPoint(4,343.315,227.679);
+   cutalpha2->SetPoint(5,28.2075,1138.39);
+   cutalpha2->SetPoint(6,158.597,1227.68);
+   cutalpha2->SetPoint(7,115.134,1227.68);
+   cutalpha2->Draw("");
+
+auto cutalpha4 = new TCutG("CUTALPHA4",7);
+   cutalpha4->SetVarX("rdtH[3]");
+   cutalpha4->SetVarY("");
+   cutalpha4->SetTitle("Graph");
+   cutalpha4->SetFillStyle(1000);
+   cutalpha4->SetPoint(0,49.9391,1111.61);
+   cutalpha4->SetPoint(1,332.45,200.893);
+   cutalpha4->SetPoint(2,2538.2,58.0357);
+   cutalpha4->SetPoint(3,2646.86,308.036);
+   cutalpha4->SetPoint(4,538.899,772.321);
+   cutalpha4->SetPoint(5,39.0733,1111.61);
+   cutalpha4->SetPoint(6,49.9391,1111.61);
+   cutalpha4->Draw("");
+
+//Histograms
+TH1F* coinTimeH = new TH1F("coinTimeH","coinTimeH",1000,-1000,1000);
+
+TH1F* eH[24];
+for (auto i = 0; i < 24; ++i)
+      eH[i] = new TH1F(Form("eH[%i]", i), Form("eH[%i]", i), 1000, -2, 18); 
+
+TH2F* rdtH[4];
+for (auto i = 0; i < 4; ++i)
+      rdtH[i] = new TH2F(Form("rdtH[%i]", i), Form("rdtH[%i]", i), 1000, 0, 6000, 1000, 0, 6000);
+
+TH1F* exH[24];
+for (auto i = 0; i < 24; i++)
+      exH[i] = new TH1F(Form("exH[%i]", i), Form("exH[%i]", i), 1000, -2, 18); 
+
+TH1F* exTotalH = new TH1F("exTotalH","exTotalH",300,-2,18);      
+
+
+ Long64_t nentries = tree->GetEntries();
+ std::cout<<" Number of entries : "<<nentries<<"\n";
+   for (Long64_t i=0;i<nentries;i++) {
+     tree->GetEntry(i);
+
+    if(i % 1000000 == 0){
+         std::cout<<" Entry number : "<<i<<"\n";
+         //if(!std::isnan(e[2])) std::cout<<" Energy index 0 "<<e[2]<<"\n"; 
+    }   
+
+     if(coinTime<-10 || coinTime>20) continue; 
+
+      coinTimeH->Fill(coinTime);
+
+      if (x < -0.95 || x > 0.95 || thetaCM < 10 || e[detID] < 1)
+        continue;
+
+      bool passLitiumCut = cutLitium1->IsInside(rdt[0], rdt[1]) || cutLitium2->IsInside(rdt[2], rdt[3]) || cutLitium3->IsInside(rdt[4], rdt[5]) || cutLitium4->IsInside(rdt[6], rdt[7]);
+      bool passAlphaCut = cutalpha1->IsInside(rdt[0], rdt[1]) || cutalpha2->IsInside(rdt[2], rdt[3]) || cutalpha3->IsInside(rdt[4], rdt[5]) || cutalpha4->IsInside(rdt[6], rdt[7]);
+
+        if (!passLitiumCut && !passAlphaCut)
+            continue;
+
+      exTotalH->Fill(Ex);
+
+     for(auto i=0;i<24;++i){
+        eH[i]->Fill(e[i]);
+        if(detID==i) exH[i]->Fill(Ex);
+    }
+
+     for(auto i=0;i<4;++i)
+        rdtH[i]->Fill(rdt[i*2],rdt[i*2+1]);
+
+  }//events
+
+gROOT->SetBatch(kFALSE);
+
+/*
+  TCanvas *c1 = new TCanvas();
+  c1->Divide(6,4);
+    for(auto i=0;i<24;++i){
+      c1->cd(i+1);
+      exH[i]->Draw();
+    }
+  
+
+  TCanvas *c2 = new TCanvas();
+  c2->Divide(6,4);
+    for(auto i=0;i<24;++i){
+      c2->cd(i+1);
+      eH[i]->Draw();
+    }
+
+  TCanvas *c3 = new TCanvas();
+  c3->Divide(2,2);
+    for(auto i=0;i<4;++i){
+      c3->cd(i+1);
+      rdtH[i]->Draw("colz");
+    } 
+
+  TCanvas *c4 = new TCanvas();
+  coinTimeH->Draw();
+
+*/
